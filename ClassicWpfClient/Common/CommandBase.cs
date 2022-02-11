38d204d9eba638f9b18955aa0eedc470fe224654
Luke Chen <chen.cai@omniremotes.com>
2022-02-11 16:04:13 +0800

    feat: add CommandBase

---
+++ ClassicWpfClient/Common/CommandBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassicWpfClient.Common
+{
+    public class CommandBase : System.Windows.Input.ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return (DoCanExecute?.Invoke(parameter) == true);
+        }
+
+        public void Execute(object? parameter)
+        {
+            DoExcute?.Invoke(parameter);
+        }
+
+        public Action<object> DoExcute { get; set; }
+
+        public Func<object, bool> DoCanExecute { get; set; }
+    }
+}
---
+++ ClassicWpfClient/ViewModel/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ClassicWpfClient.Common;
+
+namespace ClassicWpfClient.ViewModel
+{
+    public class LoginViewModel
+    {
+        public CommandBase CloseWindowCommand { get; set; }
+
+        public LoginViewModel()
+        {
+            CloseWindowCommand = new CommandBase();
+        }
+    }
+}
